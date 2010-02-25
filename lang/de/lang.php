6eecc93214424565d763c562f83a9b8b357b4384
Dominik Eckelmann <eckelmann@cosmocode.de>
2010-02-25 17:31:01 +0100

    added german translation

---
+++ lang/de/lang.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * German language file for do plugin
+ *
+ * @author Dominik Eckelmann <dokuwiki@cosmocode.de>
+ */
+
+// menu entry for admin plugins
+// $lang['menu'] = 'Your menu entry';
+
+$lang['title1'] = 'Aufgabe ist %1$s zugeordnet und ist am %2$s fällig.';
+$lang['title2'] = 'Aufgabe ist %1$s zugeordnet. Kein Fälligkeitsdatum.';
+$lang['title3'] = 'Aufgabe ist nicht zugeordnet und ist am %2$s fällig.';
+$lang['title4'] = 'Aufgabe ist nicht zugeordnet und hat kein Fälligkeitsdatum';
+
+$lang['task']   = 'Aufgabe';
+$lang['user']   = 'zugewiesen an';
+$lang['date']   = 'Fällig am';
+$lang['status'] = 'Status';
+
+$lang['js']['done']             = 'Diese Aufgabe wurde am %s erledigt.';
+$lang['js']['open']             = 'Diese Aufgabe ist noch nicht erledigt.';
+$lang['js']['toolbar_title']    = 'Neue Aufgabe erstellen';
+$lang['js']['popup_assign']     = 'Zuweisen an (Benutzername)';
+$lang['js']['popup_date']       = 'Fällig am (YYYY-MM-DD)';
+$lang['js']['popup_submit']     = 'Einfügen';
+$lang['js']['popup_title']      = 'Aufgabe erstellen';
+
+//Setup VIM: ex: et ts=4 enc=utf-8 :
