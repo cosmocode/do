3e14f5e7a45129ebaa2b69a133a66b538231d513
Robert Bogenschneider <bogi@uea.org>
2013-10-08 11:45:36 +0200

    translation update

---
+++ lang/eo/lang.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Robert Bogenschneider <bogi@uea.org>
+ */
+$lang['js']['title1']          = 'Tasko donita al %1$s. Limdato je %2$s.';
+$lang['js']['title2']          = 'Tasko donita al %1$s. Neniu limdato.';
+$lang['js']['title3']          = 'Nedonita tasko kun limdato %2$s.';
+$lang['js']['title4']          = 'Nedonita tasko, neniu limdato.';
+$lang['js']['title_done']      = 'Ĉiuj taskoj plenumiĝis.';
+$lang['js']['title_undone']    = 'Ekzistas neplenumitaj taskoj.';
+$lang['js']['done']            = 'Tiu tasko plenumiĝis je %s.';
+$lang['js']['open']            = 'Tiu tasko ankoraŭ ne plenumiĝis.';
+$lang['js']['toolbar_title']   = 'Enmeti novan taskon';
+$lang['js']['popup_assign']    = 'Doni al <small>(uzantonomo(j))</small>';
+$lang['js']['popup_date']      = 'Limdato je <small>(JJJJ-MM-TT)</small>';
+$lang['js']['popup_submit']    = 'Enmeti';
+$lang['js']['popup_title']     = 'Krei taskon';
+$lang['js']['popup_msg']       = 'Kompletiga mesaĝo';
+$lang['js']['note_done']       = 'Kompleta:';
+$lang['js']['assignee']        = 'Tasko donita al %s.';
+$lang['js']['due']             = 'Limdato je %s.';
+$lang['js']['notloggedin']     = 'Vi ne ensalutis.';
+$lang['js']['closedby']        = 'La tasko fermiĝis de %s.';
+$lang['js']['by_unknown']      = 'nekonata uzanto';
+$lang['js']['finish_popup_title'] = 'Fermi taskon';
+$lang['js']['finish_popup_submit'] = 'Fermi';
+$lang['title_intime']          = 'Ekzistas %1$d malfermaj taskoj.';
+$lang['title_late']            = 'Ekzistas %1$d malfermaj taskoj, %2$d malfruas.';
+$lang['title_alldone']         = 'Ĉiuj taskoj plenumitaj.';
+$lang['task']                  = 'Tasko';
+$lang['user']                  = 'Donita al';
+$lang['date']                  = 'Limdato je';
+$lang['status']                = 'Statuso';
+$lang['creator']               = 'Kreinto';
+$lang['none']                  = 'Neniuj taskoj trovitaj';
+$lang['mail_open']             = 'Nova tasko: %s';
+$lang['mail_reopen']           = 'Remalfermita: %s';
+$lang['mail_close']            = 'Tasko fermita: %s';
+$lang['someone']               = 'Iu';
+$lang['nodue']                 = 'Neniu';
