a267ff93142f7f8dcbc99142ceca983d48b3d653
Hideaki SAWADA <chuno@live.jp>
2013-10-28 12:50:08 +0100

    translation update

---
+++ lang/ja/lang.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Hideaki SAWADA <chuno@live.jp>
+ */
+$lang['js']['title1']          = '%1$s が担当・%2$s が期限の作業';
+$lang['js']['title2']          = '%1$s が担当・期限なしの作業';
+$lang['js']['title3']          = '担当が未定・%2$s が期限の作業';
+$lang['js']['title4']          = '担当が未定・期限なしの作業';
+$lang['js']['title_done']      = '全作業が完了しています。';
+$lang['js']['title_undone']    = '未完の作業があります。';
+$lang['js']['done']            = 'この作業は %s に完了しました。';
+$lang['js']['open']            = 'この作業は未完です。';
+$lang['js']['toolbar_title']   = '新規作業の作成';
+$lang['js']['popup_assign']    = '担当者<small>（ユーザー名）</small>';
+$lang['js']['popup_date']      = '期日<small>（YYYY-MM-DD）</small>';
+$lang['js']['popup_submit']    = '作成';
+$lang['js']['popup_title']     = '作業の作成';
+$lang['js']['popup_msg']       = '完了通知';
+$lang['js']['note_done']       = '完了：';
+$lang['js']['assignee']        = '担当者は %s です。';
+$lang['js']['due']             = '期日は %s です。';
+$lang['js']['notloggedin']     = 'ログインしていません。';
+$lang['js']['closedby']        = '%s が作業を完了しました。';
+$lang['js']['by_unknown']      = '不明なユーザー';
+$lang['js']['finish_popup_title'] = '作業の完了';
+$lang['js']['finish_popup_submit'] = '完了';
+$lang['title_intime']          = '未完の作業が %1$d 件あります。';
+$lang['title_late']            = '未完の作業が %1$d 件、遅延の作業が %2$d 件あります。';
+$lang['title_alldone']         = '全作業が完了しています。';
+$lang['task']                  = '作業';
+$lang['user']                  = '担当者';
+$lang['date']                  = '期日';
+$lang['status']                = '状態';
+$lang['creator']               = '作成者';
+$lang['none']                  = '作業がありません';
+$lang['mail_open']             = '新規作業：%s';
+$lang['mail_reopen']           = '再開作業：%s';
+$lang['mail_close']            = '作業完了：%s';
+$lang['someone']               = '未定';
+$lang['nodue']                 = '期限なし';
---
+++ lang/ja/settings.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Hideaki SAWADA <chuno@live.jp>
+ */
+$lang['userpage']              = 'ユーザーページヘのリンクを作成するための printf 入力文字列（ユーザー名が表示される場所に ›%s‹ を挿入する）';
+$lang['notify_assignee']       = '関係ユーザーに新規作業や状態変更のメールを送信する。';
