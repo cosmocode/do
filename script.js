d5370adefc0ff79ca34b2f9814b2c28957d8df60
Michael Große <grosse@cosmocode.de>
2017-04-28 16:50:28 +0200

    Split up general.js and fix missing tb-button #34
    The bug was introduced in 20a437a833b when the script-loading code
    was wrapped inside a function that executed only on document-ready.
    
    SPR-975

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+/* DOKUWIKI:include scripts/PluginDo.js */
+/* DOKUWIKI:include scripts/toolbarButton.js */
 jQuery(function () {
     /* DOKUWIKI:include scripts/general.js */
     /* DOKUWIKI:include scripts/userTasksOverlay.js */
