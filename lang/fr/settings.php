d31a8890e8310590cd7a5ad9cab29fe70f7cb6da
ubibene <services.m@benard.info>
2017-05-30 16:35:23 +0200

    translation update

--- lang/fr/settings.php
+++ lang/fr/settings.php
@@ -1,10 +1,10 @@
 <?php
+
 /**
- * French language file for do plugin
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
  * @author Adrian Lang <lang@cosmocode.de>
  * @author Agnès Haasser <agnes.haasser@gmail.com>
  */
-
-$lang['userpage'] = 'Une chaîne printf utilisée pour générer un lien vers une page d’utilisateur (écrivez ›%s‹ là où le nom d’utilisateur doit apparaître.)';
-$lang['notify_assignee'] = 'Prévenir par e-mail les utilisateurs à qui les tâches sont affectées';
+$lang['userpage']              = 'Une chaîne printf utilisée pour générer un lien vers une page d’utilisateur (écrivez ›%s‹ là où le nom d’utilisateur doit apparaître.)';
+$lang['notify_assignee']       = 'Prévenir par e-mail les utilisateurs à qui les tâches sont affectées';
