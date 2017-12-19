5961a7f0e97b0f515acc548d4f006eff6c8b9ff1
Tor Härnqvist <tor@harnqvist.se>
2017-12-19 06:00:09 +0100

    translation update

---
+++ lang/sv/lang.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Tor Härnqvist <tor@harnqvist.se>
+ */
+$lang['js']['title1']          = 'Uppgift tilldelad till %1$s. Förfaller %2$s.';
+$lang['js']['title2']          = 'Uppgift tilldelad till %1$s. Ingen tidsgräns.';
+$lang['js']['title3']          = 'Ej tilldelad uppgift, förfaller %2$d.';
+$lang['js']['title4']          = 'Ej tilldelad uppgift, ingen tidsgräns.';
+$lang['js']['title_done']      = 'Alla uppgifter avklarade.';
+$lang['js']['title_undone']    = 'Det finns %1$d öppna uppgifter.';
+$lang['js']['title_late']      = 'Det finns %1$d öppna uppgifter, %2$d är försenade.';
+$lang['js']['done']            = 'Denna uppgift avklarades på %s';
+$lang['js']['open']            = 'Denna uppgift är ännu ej avklarad.';
+$lang['js']['toolbar_title']   = 'Lägg till ny uppgift';
+$lang['js']['popup_assign']    = 'Tilldela till <small>(användarnamn)</small>';
+$lang['js']['popup_date']      = 'Förfallodatum <small>(ÅÅÅÅ-MM-DD)</small>';
+$lang['js']['popup_submit']    = 'Infoga';
+$lang['js']['popup_title']     = 'Skapa uppgift';
+$lang['js']['popup_msg']       = 'Avklarandemeddelande';
+$lang['js']['note_done']       = 'Avklarat:';
+$lang['js']['assignee']        = 'Uppgiften är tilldelad till %s.';
+$lang['js']['due']             = 'Förfaller %s';
+$lang['js']['notloggedin']     = 'Du är inte inloggad.';
+$lang['js']['notallowed']      = 'Åtgärden är ej tillåten';
+$lang['js']['closedby']        = 'Uppgiften stängdes av %s.';
+$lang['js']['by_unknown']      = 'en okänd användare';
+$lang['js']['finish_popup_title'] = 'Stäng uppgift';
+$lang['js']['finish_popup_submit'] = 'Stäng';
+$lang['title_intime']          = 'Det finns %1$d öppna uppgifter.';
+$lang['title_late']            = 'Det finns %1$d öppna uppgifter, %2$d är försenade.';
+$lang['title_alldone']         = 'Alla uppgifter avklarade.';
+$lang['tasks_user_none']       = 'Du har inga öppna uppgifter.';
+$lang['tasks_user_intime']     = 'Du har %1$d öppna uppgifter.';
+$lang['prefix_tasks_user']     = 'dina öppna uppgifter:';
+$lang['task']                  = 'Uppgift';
+$lang['user']                  = 'Tilldelad till';
+$lang['date']                  = 'Förfallodatum';
+$lang['status']                = 'Status';
+$lang['creator']               = 'Skapare';
+$lang['none']                  = 'Inga uppgifter hittades';
+$lang['mail_open']             = 'Ny uppgift: %s';
+$lang['mail_reopen']           = 'Återöppnad: %s';
+$lang['mail_close']            = 'Uppgift stängd: %s';
+$lang['someone']               = 'Någon';
+$lang['nodue']                 = 'Ingen';
