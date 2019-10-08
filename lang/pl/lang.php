059bba729299ce5a32c9efa5b13003ce5a8a8565
Bartek S <sadupl@gmail.com>
2019-10-08 20:00:35 +0200

    translation update

---
+++ lang/pl/lang.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Bartek S <sadupl@gmail.com>
+ */
+$lang['js']['title_done']      = 'Wszystkie zadania wykonane.';
+$lang['js']['toolbar_title']   = 'Dodaj nowe zadanie.';
+$lang['js']['popup_submit']    = 'Wstaw';
+$lang['js']['popup_title']     = 'Utwórz zadanie';
+$lang['js']['popup_msg']       = 'Wiadomość o zakończeniu';
+$lang['js']['note_done']       = 'Ukończono:';
+$lang['js']['due']             = 'Ze względu na %s.';
+$lang['js']['notloggedin']     = 'Nie jesteś zalogowany.';
+$lang['js']['notallowed']      = 'Działanie niedozwolone';
+$lang['js']['closedby']        = 'Zadanie zostało zamknięte przez %s.';
+$lang['js']['by_unknown']      = 'nieznany użytkownik';
+$lang['js']['finish_popup_title'] = 'Zamknij zadanie';
+$lang['js']['finish_popup_submit'] = 'Zamknij';
+$lang['title_alldone']         = 'Wszystkie zadania ukończone.';
+$lang['tasks_user_none']       = 'Nie masz otwartych zadań.';
+$lang['tasks_user_intime']     = 'Masz %1$d otwartych zadań.';
+$lang['prefix_tasks_user']     = 'twoje otwarte zadania:';
+$lang['task']                  = 'Zadanie';
+$lang['user']                  = 'Przypisane do';
+$lang['date']                  = 'Z powodu';
+$lang['status']                = 'Status';
+$lang['creator']               = 'Twórca';
+$lang['none']                  = 'Nie znaleziono zadań';
+$lang['mail_open']             = 'Nowe zadanie: %s';
+$lang['mail_close']            = 'Zadanie zamknięte: %s';
+$lang['someone']               = 'Ktoś';
+$lang['nodue']                 = 'Żaden';
---
+++ lang/pl/settings.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Bartek S <sadupl@gmail.com>
+ */
+$lang['notify_assignee']       = 'Wysyłaj wiadomości e-mail o nowych zadaniach i zmianach statusu do dotkniętych użytkowników';
