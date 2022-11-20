e1039e5a5110f5dff915fd8e6e5b849420079f4d
nerun <nerunblogs@gmail.com>
2022-11-20 14:34:13 -0300

    Added brazilian portuguese (pt-br) translation.

---
+++ lang/pt-br/lang.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * Brazilian portuguese language file for do plugin
+ *
+ * @author Daniel Dias Rodrigues <danieldiasr@gmail.com>
+ */
+
+
+$lang['js']['title1'] = 'Tarefa atribuída a %1$s. Vence em %2$s.';
+$lang['js']['title2'] = 'Tarefa atribuída a %1$s. Sem prazo.';
+$lang['js']['title3'] = 'Tarefa não atribuída, vence em %2$s.';
+$lang['js']['title4'] = 'Tarefa não atribuída, sem prazo.';
+
+$lang['title_intime']      = 'Há %1$d tarefa(s) aberta(s).';
+$lang['title_late']        = 'Há %1$d tarefa(s) aberta(s), %2$d atrasada(s).';
+$lang['title_alldone']     = 'Todas as tarefas concluídas.';
+$lang['tasks_user_none']   = 'Você não tem tarefas abertas.';
+$lang['tasks_user_intime'] = 'Você tem %1$d tarefa(s) aberta(s).';
+$lang['prefix_tasks_user'] = 'suas tarefas abertas: ';
+
+$lang['js']['title_done']   = 'Todas as tarefas concluídas.';
+$lang['js']['title_undone'] = 'Há %1$d tarefa(s) aberta(s).';
+$lang['js']['title_late']   = 'Há %1$d tarefa(s) aberta(s), %2$d atrasada(s).';
+
+$lang['task']    = 'Tarefa';
+$lang['user']    = 'Atribuída a';
+$lang['date']    = 'Vence em';
+$lang['status']  = 'Situação';
+$lang['creator'] = 'Criador';
+
+$lang['none'] = 'Nenhuma tarefa encontrada';
+
+$lang['js']['done']          = 'Esta tarefa foi concluída em %s.';
+$lang['js']['open']          = 'Esta tarefa ainda não foi concluída.';
+$lang['js']['toolbar_title'] = 'Inserir nova tarefa';
+$lang['js']['popup_assign']  = 'Atribuir a <small>(usuário(s))</small>';
+$lang['js']['popup_date']    = 'Vence em <small>(AAAA-MM-DD)</small>';
+$lang['js']['popup_submit']  = 'Inserir';
+$lang['js']['popup_title']   = 'Criar tarefa';
+$lang['js']['popup_msg']     = 'Mensagem de conclusão';
+$lang['js']['note_done']     = 'Concluído: ';
+
+$lang['js']['assignee'] = 'Tarefa atribuída a %s.';
+$lang['js']['due']      = 'Vence em %s.';
+
+$lang['js']['notloggedin'] = 'Você não está logado.';
+$lang['js']['notallowed']  = 'Ação não permitida';
+$lang['js']['closedby']    = 'A tarefa foi encerrada por %s.';
+$lang['js']['by_unknown']  = 'um usuário desconhecido';
+
+$lang['js']['finish_popup_title']  = 'Fechar tarefa';
+$lang['js']['finish_popup_submit'] = 'Fechar';
+
+$lang['mail_open']   = 'Nova tarefa: %s';
+$lang['mail_reopen'] = 'Reaberto: %s';
+$lang['mail_close']  = 'Tarefa encerrada: %s';
+
+$lang['someone'] = 'Alguém';
+$lang['nodue']   = 'Nenhum';
