0729b9969a4aef724cb6777eb05b02b3e4ff09df
VladPolskiy <polskiyvn@gmail.com>
2023-08-19 03:55:08 +0200

    translation update

--- lang/ru/lang.php
+++ lang/ru/lang.php
@@ -3,14 +3,18 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author VladPolskiy <polskiyvn@gmail.com>
  * @author Aleksandr Selivanov <alexgearbox@yandex.ru>
  */
-$lang['js']['title1']          = 'Назначена для %1$s. Срок: до %2$s.';
-$lang['js']['title2']          = 'Назначена для %1$s. Без крайнего срока.';
+$lang['js']['title1']          = 'Задача назначена для %1$s. Срок: до %2$s.';
+$lang['js']['title2']          = 'Задача назначена для %1$s. Без крайнего срока.';
 $lang['js']['title3']          = 'Никому не назначенная задача сроком до %2$s.';
 $lang['js']['title4']          = 'Никому не назначена; без крайнего срока.';
 $lang['js']['title_done']      = 'Все задачи выполнены.';
+$lang['js']['title_undone']    = 'Есть открытые задачи: %1$d.';
+$lang['js']['title_late']      = 'Открыто %1$d задача, %2$d просрочено.';
 $lang['js']['done']            = 'Задача завершена %s.';
+$lang['js']['open']            = 'Эта задача еще не выполнена.';
 $lang['js']['toolbar_title']   = 'Добавить новую задачу';
 $lang['js']['popup_assign']    = 'Назначить для <small>(имя пользователя (-ей))</small>';
 $lang['js']['popup_date']      = 'Срок: до <small>(YYYY-MM-DD)</small>';
@@ -23,10 +27,15 @@ $lang['js']['due']             = 'Срок: до %s.';
 $lang['js']['notloggedin']     = 'Вы не залогинены.';
 $lang['js']['notallowed']      = 'Дейстивие не разрешено';
 $lang['js']['closedby']        = 'Задачу закрыл (-а) %s.';
+$lang['js']['by_unknown']      = 'неизвестный пользователь';
 $lang['js']['finish_popup_title'] = 'Закрыть задачу';
 $lang['js']['finish_popup_submit'] = 'Закрыть';
+$lang['title_intime']          = 'Есть открытые задачи: %1$d.';
+$lang['title_late']            = 'Открыто %1$d задача, %2$d просрочено.';
 $lang['title_alldone']         = 'Все задачи выполнены.';
 $lang['tasks_user_none']       = 'У вас нет открытых задач.';
+$lang['tasks_user_intime']     = 'У вас есть открытые задачи: %1$d.';
+$lang['prefix_tasks_user']     = 'Ваши открытые задачи:';
 $lang['task']                  = 'Задача';
 $lang['user']                  = 'Назначена для';
 $lang['date']                  = 'Срок: до';
---
+++ lang/ru/settings.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author VladPolskiy <polskiyvn@gmail.com>
+ */
+$lang['userpage']              = 'Входная строка printf, используемая для создания ссылки на страницу пользователя (вставьте ›%s‹ там, где должно отображаться имя пользователя)';
+$lang['notify_assignee']       = 'Отправлять письма о новых задачах и изменениях статуса затронутым пользователям';
